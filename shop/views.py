81d39c7e25aa76083bf516f43c14a31c7c68eba3
AnshikaDixit <dixitanshika09@gmail.com>
2024-09-07 12:12:14 +0530

    ADDED --> pipeline and pages in shop app

--- shop/views.py
+++ shop/views.py
@@ -3,4 +3,28 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Index Shop")
+    return render(request, "shop/index.html")
+
+def about(request):
+    return HttpResponse("We are at About")
+    # return render(request, "shop/index.html")
+
+def search(request):
+    return HttpResponse("We are at search")
+    # return render(request, "shop/index.html")
+
+def contact(request):
+    # return render(request, "shop/index.html")
+     return HttpResponse("We are at Contact")
+
+def tracker(request):
+    # return render(request, "shop/index.html")
+     return HttpResponse("We are at Tracker")
+
+def prodView(request):
+    # return render(request, "shop/index.html")
+     return HttpResponse("We are at Prod View")
+
+def checkout(request):
+    # return render(request, "shop/index.html")
+      return HttpResponse("We are at Checkout")
