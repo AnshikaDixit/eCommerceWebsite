5dc6abeff3c1e69fc03f31344675b198476cc814
AnshikaDixit <dixitanshika09@gmail.com>
2024-09-10 11:23:20 +0530

    ADDED --> filter to show category products list

--- shop/views.py
+++ shop/views.py
@@ -1,9 +1,35 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
+from math import ceil
 
 # Create your views here.
 def index(request):
-    return render(request, "shop/index.html")
+    # products= Product.objects.all()
+    allProds=[]
+    catprods= Product.objects.values('category', 'id')
+    print("catProds ==>")
+    print({catprods})
+    cats= {item["category"] for item in catprods}
+    print("cats ==>")
+    print(cats)
+    for cat in cats:
+        print("cat ==>")
+        print(cat)
+        prod=Product.objects.filter(category=cat)
+        print("prod ==>")
+        print(prod)
+        n = len(prod)
+        nSlides = n // 4 + ceil((n / 4) - (n // 4))
+        print("nslides ==>")
+        print(nSlides)
+        allProds.append([prod, range(1, nSlides), nSlides])
+        print("allProds ==>")
+        print(allProds)
+
+
+    params={'allProds':allProds }
+    return render(request,"shop/index.html", params)
 
 def about(request):
     # return HttpResponse("We are at About")
