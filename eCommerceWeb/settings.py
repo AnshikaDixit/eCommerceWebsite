7d9bd63f48edf2447591e6540e3641f8f52b9e48
AnshikaDixit <dixitanshika09@gmail.com>
2024-09-12 12:36:25 +0530

    ADDED --> Tracking order details

--- eCommerceWeb/settings.py
+++ eCommerceWeb/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'eCommerceWeb.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['eCommerceWeb/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- shop/models.py
+++ shop/models.py
@@ -13,7 +13,6 @@ class Product(models.Model):
 
     def __str__(self):
         return self.product_name
-    
 
 class Contact(models.Model):
     msg_id = models.AutoField(primary_key=True)
@@ -24,14 +23,22 @@ class Contact(models.Model):
 
     def __str__(self):
         return self.name
-    
 class Orders(models.Model):
     order_id = models.AutoField(primary_key=True)
     items_json = models.CharField(max_length=5000)
     name = models.CharField(max_length=90)
-    email = models.CharField(max_length=100)
-    address = models.CharField(max_length=100)
-    city = models.CharField(max_length=100)
-    state = models.CharField(max_length=100)
-    zipcode = models.CharField(max_length=100)
-    phone = models.CharField(max_length=100, default="")
+    email = models.CharField(max_length=111)
+    address = models.CharField(max_length=111)
+    city = models.CharField(max_length=111)
+    state = models.CharField(max_length=111)
+    zipcode = models.CharField(max_length=111)
+    phone = models.CharField(max_length=111, default="")
+
+class OrderUpdate(models.Model):
+    update_id  = models.AutoField(primary_key=True)
+    order_id = models.IntegerField(default="")
+    update_desc = models.CharField(max_length=5000)
+    timestamp = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.update_desc[0:7] + "..."
