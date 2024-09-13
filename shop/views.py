7cc90a0161d4a5510198f944e5c986d09b7bb49c
AnshikaDixit <dixitanshika09@gmail.com>
2024-09-13 18:05:33 +0530

    FIX --> tracker order bug fixed

--- shop/views.py
+++ shop/views.py
@@ -2,10 +2,8 @@ from django.shortcuts import render
 from .models import Product, Contact, Orders, OrderUpdate
 from math import ceil
 import json
-
 # Create your views here.
 from django.http import HttpResponse
-
 
 def index(request):
     allProds = []
@@ -20,27 +18,28 @@ def index(request):
     return render(request, 'shop/index.html', params)
 
 def searchMatch(query, item):
-    # return true only if query matches item
+    '''return true only if query matches the item'''
     if query in item.desc.lower() or query in item.product_name.lower() or query in item.category.lower():
-       return True
+        return True
     else:
-       return False
+        return False
 
 def search(request):
-    query= request.GET.get('search')
+    query = request.GET.get('search')
     allProds = []
     catprods = Product.objects.values('category', 'id')
     cats = {item['category'] for item in catprods}
     for cat in cats:
         prodtemp = Product.objects.filter(category=cat)
-        prod=[item for item in prodtemp if searchMatch(query, item)]
+        prod = [item for item in prodtemp if searchMatch(query, item)]
+
         n = len(prod)
         nSlides = n // 4 + ceil((n / 4) - (n // 4))
-        if len(prod)!= 0:
+        if len(prod) != 0:
             allProds.append([prod, range(1, nSlides), nSlides])
-    params = {'allProds': allProds, "msg":""}
-    if len(allProds)==0 or len(query)<4:
-        params={'msg':"Please make sure to enter relevant search query"}
+    params = {'allProds': allProds, "msg": ""}
+    if len(allProds) == 0 or len(query)<4:
+        params = {'msg': "Please make sure to enter relevant search query"}
     return render(request, 'shop/search.html', params)
 
 
@@ -72,12 +71,12 @@ def tracker(request):
                 updates = []
                 for item in update:
                     updates.append({'text': item.update_desc, 'time': item.timestamp})
-                    response = json.dumps([updates, order[0].items_json], default=str)
+                    response = json.dumps({"status":"success", "updates": updates, "itemsJson": order[0].items_json}, default=str)
                 return HttpResponse(response)
             else:
-                return HttpResponse('{}')
+                return HttpResponse('{"status":"noitem"}')
         except Exception as e:
-            return HttpResponse('{}')
+            return HttpResponse('{"status":"error"}')
 
     return render(request, 'shop/tracker.html')
 
